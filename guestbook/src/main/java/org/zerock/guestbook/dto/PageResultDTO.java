effced96a139e15d8e74f4106f097104b56f67a6
jh4dev <jh4dev@gmail.com>
2022-09-22 22:36:31 +0900

    페이징 관련

--- guestbook/src/main/java/org/zerock/guestbook/dto/PageResultDTO.java
+++ guestbook/src/main/java/org/zerock/guestbook/dto/PageResultDTO.java
@@ -5,10 +5,12 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * @Desc Page<Entity> 의 entity 객체들을 DTO 객체로 변환하여 자료구조로 담아야 함
@@ -16,11 +18,38 @@ import java.util.stream.Collectors;
 @Data
 public class PageResultDTO<DTO, EN>{
 
+    //DTO리스트
     private List<DTO> dtoList;
 
-    public PageResultDTO(Page<EN> result, Function<EN, DTO> fn) {
+    private int totalPage;              //총 페이지 수
+    private int page;                   //현재 페이지 번호
+    private int size;                   //목록 사이즈
+    private int start, end;             //시작 페이지 번호, 끝 페이지 번호
+    private boolean prev, next;         //이전, 다음
+    private List<Integer> pageList;     //페이지 번호 목록
 
-        dtoList = result.stream().map(fn).collect(Collectors.toList());
+    private void makePageList(Pageable pageable) {
+
+        this.page = pageable.getPageNumber() + 1;
+        this.size = pageable.getPageSize();
+
+        int tempEnd = (int) (Math.ceil(page/10.0) * 10);
+
+        start = tempEnd -9;
+        prev = start > 1;
+        end = totalPage > tempEnd ? tempEnd : totalPage;
+        next = totalPage > tempEnd;
+
+        pageList = IntStream.rangeClosed(start, end).boxed().collect(Collectors.toList());
     }
+
+    public PageResultDTO(Page<EN> result, Function<EN, DTO> fn) {
+        dtoList = result.stream().map(fn).collect(Collectors.toList());
+
+        totalPage = result.getTotalPages();
+        makePageList(result.getPageable());
+    }
+
+
 
 }
--- guestbook/src/test/java/org/zerock/guestbook/service/GuestbookServiceTests.java
+++ guestbook/src/test/java/org/zerock/guestbook/service/GuestbookServiceTests.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.zerock.guestbook.dto.GuestbookDTO;
+import org.zerock.guestbook.dto.PageRequestDTO;
+import org.zerock.guestbook.dto.PageResultDTO;
+import org.zerock.guestbook.entity.Guestbook;
 import org.zerock.guestbook.repository.GuestbookRepository;
 
 @SpringBootTest
@@ -22,5 +25,41 @@ public class GuestbookServiceTests {
                 .build();
 
         System.out.println(guestbookService.register(dto));
+    }
+
+    @Test
+    void testList() {
+
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
+                .page(1)
+                .size(10)
+                .build();
+
+        PageResultDTO<GuestbookDTO, Guestbook> resultDTO = guestbookService.getList(pageRequestDTO);
+
+        for(GuestbookDTO guestbookDTO : resultDTO.getDtoList()) {
+            System.out.println(guestbookDTO.toString());
+        }
+    }
+
+    @Test
+    void testList2() {
+
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
+                .page(1)
+                .size(10)
+                .build();
+
+        PageResultDTO<GuestbookDTO, Guestbook> resultDTO = guestbookService.getList(pageRequestDTO);
+
+        System.out.println("PREV : " + resultDTO.isPrev());
+        System.out.println("NEXT : " + resultDTO.isNext());
+        System.out.println("TOTAL : " + resultDTO.getTotalPage());
+        System.out.println("===================================");
+        for(GuestbookDTO guestbookDTO : resultDTO.getDtoList()) {
+            System.out.println(guestbookDTO);
+        }
+        System.out.println("===================================");
+        resultDTO.getPageList().forEach(i -> System.out.println(i));
     }
 }
